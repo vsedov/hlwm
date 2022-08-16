6f96fdb627e2f302c2d99b0086941e2aff226dda
vsedov <viv.sedov@hotmail.com>
2022-08-17 00:46:32 +0100

    Create a setup folder where magic items will be called from

---
+++ herbstluftwm/setup/magic/key_binder.py
@@ -0,0 +1,24 @@
+from herbstluftwm.log import get_logger
+
+log = get_logger(__name__)
+
+
+def setup_dict():
+
+    return {
+        "spawn": ["spawn", " "],
+        "mod": ["Mod4", "-"],
+        "shift": ["Shift", "-"],
+        "ctrl": ["control", "-"],
+        "alt": ["alt", "-"],
+        "space": ["space", "-"],
+    }
+
+
+def setup_extra():
+    return {
+        "mod_shift": [hc.mod(hc.shift()), "-"],
+        "mod_ctrl": [hc.mod(hc.ctrl()), "-"],
+        "mod_alt": [hc.mod(hc.alt()), "-"],
+        "mod_shift_ctrl": [hc.mod(hc.shift(hc.ctrl())), "-"],
+    }
