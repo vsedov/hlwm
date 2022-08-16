a23d34f1b58124f053f33c6ec669710e899aaa17
vsedov <viv.sedov@hotmail.com>
2022-08-17 00:47:24 +0100

    update constants

--- herbstluftwm/__main__.py
+++ herbstluftwm/__main__.py
@@ -1,42 +1,9 @@
-import herbstluftwm.constants as const
+from herbstluftwm.helper.magic import hc
 from herbstluftwm.log import get_logger
-from herbstluftwm.helper.utils import (
-    bind_cycle_layout,
-    chain,
-    do_config,
-    hc,
-    rotate,
-    startup_run,
-    system_run,
-)
+from herbstluftwm.setup.main import inital_setup
 
 log = get_logger(__name__)
+inital_setup()
 
-tag_names = list(range(1, 10))
-tag_keys = list(range(1, 10)) + [0]
-
-hc("emit_hook reload")
-system_run("echo 35 > /tmp/herbstluftwm-gap")
-hc("lock")
-# standard
-# remove all existing keybindings
-hc('keyunbind --all')
-hc("mouseunbind --all")
-hc("unrule -F")
-bind_cycle_layout()
-
-layout = "(split horizontal:0.5:0 " \
-         "(clients vertical:0) (clients vertical:0))"
-hc("load " + str(tag_names[0]) + " '" + layout + "'")
-
-# tag number 5
-hc("floating 5 on")
-
-# hc("set tree_style '╾│ ├└╼─┐'")
-hc("set tree_style '⊙│ ├╰»─╮'")
-
-# unlock, just to be sure
-hc("unlock")
-
-# load on startup
-startup_run(const.STARTUP)
+#  TODO(vsedov) (00:31:34 - 17/08/22): test this shit
+log.info(hc.mod_shift("x"))
--- herbstluftwm/constants.py
+++ herbstluftwm/constants.py
@@ -11,8 +11,8 @@ TRACE_LOGGERS: list[str] = [f"!{__name__}"]
 DIR: Optional[str] = os.path.dirname(__file__)
 
 STARTUP = {
-    "applet &", "nm-applet", "dunst &", "flameshot &",
-    "feh --bg-scale ~/Downloads/upscalebuterfly.png",
-    "~/.config/polybar/launch.sh", "pasystray", "/user/bin/subl",
-    "/user/bin/franz", "/user/bin/lampe-gtk", "sleep 15 ^^ pulseeffects"
+    "applet &", "nm-applet", "dunst &", "flameshot &", "feh --bg-scale ~/Downloads/upscalebuterfly.png",
+    "~/.config/polybar/launch.sh", "pasystray", "/user/bin/subl", "/user/bin/franz", "/user/bin/lampe-gtk",
+    "sleep 15 ^^ pulseeffects"
 }
+EXCLUDE = ["get_logger"]
