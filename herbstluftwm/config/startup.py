c74f40a7b87939367ef29a0f89ad71ec4296bc7c
vsedov <viv.sedov@hotmail.com>
2022-08-17 00:45:25 +0100

    basic config setup

---
+++ herbstluftwm/config/startup.py
@@ -0,0 +1,13 @@
+import os
+
+# things to load on startup
+
+# def bind_cycle_layout(self):
+#     self.do_config(
+#         "keybind",
+#         self.mod("space") +
+#         "or , and . compare tags.focus.curframe_wcount = 2 "
+#         ". cycle_layout +1 vertical horizontal max vertical grid "
+#         ", cycle_layout +1 ")
+#
+#
